fb9d3fd01e83c171e60a7e43ef355faa58abcb86
adphillips2017 <adphillips2017@users.noreply.github.com>
2021-02-11 22:22:38 -0500

    new components

---
+++ src/components/PhotoHeader.svelte
@@ -0,0 +1,42 @@
+<div class="photo-header">
+  <div class="text">
+    <div class="header txt-heavy">More than just shorter links</div>
+    <div class="subtitle txt-gray">
+      Build your brand's recognition and get detailed insights on how your links
+      are performing.
+    </div>
+    <div class="btn btn-rounded-lg">Get Started</div>
+  </div>
+
+  <img
+    class="illustration"
+    src="build/images/illustration-working.svg"
+    alt="working.svg"
+  />
+</div>
+
+<style>
+  .photo-header {
+    display: flex;
+  }
+
+  .text {
+    width: 30rem;
+    flex-shrink: 0;
+  }
+
+  .header {
+    font-size: 4rem;
+    line-height: 4.5rem;
+    margin-bottom: 1rem;
+  }
+
+  .subtitle {
+    margin-bottom: 2rem;
+  }
+
+  .illustration {
+    margin-top: -4rem;
+    margin-left: 4rem;
+  }
+</style>
