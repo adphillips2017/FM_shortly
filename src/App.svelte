0a5e593a5c889bec2227baca422a4ea01f59f74d
adphillips2017 <adphillips2017@users.noreply.github.com>
2021-02-11 21:07:37 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Navbar from './components/Navbar.svelte';
+</script>
+
+<main>
+<div class="main-container">
+	<Navbar />
+
+</div>
+</main>
+
+<style>
+	.main-container {
+		padding-top: 2rem;
+		padding-left: 8rem;
+		padding-right: 8rem;
+	}
+</style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,38 @@
+<div class="nav">
+    <img class="logo" src="build/images/logo.svg" alt="logo.svg">
+    <div class="link txt-gray">Features</div>
+    <div class="link txt-gray">Pricing</div>
+    <div class="link txt-gray">Resources</div>
+    <div class="spacer"></div>
+    <div class="link txt-gray">Login</div>
+    <div class="btn btn-rounded">Sign Up</div>
+</div>
+
+<style>
+    .nav {
+        display: flex;
+        align-items: center;
+        font-size: .85rem;
+    }
+
+    .logo {
+        margin-right: 1rem;
+    }
+
+    .link {
+        margin-left: 1.5rem;
+        cursor: pointer;
+    }
+
+    .link:hover {
+        color: var(--cyan);
+    }
+
+    .btn {
+        margin-left: 2rem;
+    }
+
+    .spacer {
+        flex-grow: 1;
+    }
+</style>
