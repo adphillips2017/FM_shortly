990ef5b9bee56ad133f25935e193b394e135022e
adphillips2017 <adphillips2017@users.noreply.github.com>
2021-02-14 23:32:45 -0500

    made link shortener work better
    capturing shortened links and showing them below with option to copy them, which works now.

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,51 @@
 <script lang="ts">
-	import Navbar from './components/Navbar.svelte';
-	import PhotoHeader from './components/PhotoHeader.svelte';
-	import LinkShortener from './components/LinkShortener.svelte';
+  import Navbar from "./components/Navbar.svelte";
+  import PhotoHeader from "./components/PhotoHeader.svelte";
+  import LinkShortener from "./components/LinkShortener.svelte";
+  import LinkCopier from "./components/LinkCopier.svelte";
+  import type { ShortenedLink } from "./models/ShortenedLink";
+
+  let links: ShortenedLink[] = [];
+
+  const handleShortLink = (event) => {
+    links = [...links, event.detail];
+  };
 </script>
 
 <main>
-<div class="main-container">
-	<Navbar />
-	<div class="spacer-7"></div>
+  <div class="main-container">
+    <Navbar />
+    <div class="spacer-7" />
 
-	<PhotoHeader />
-	<div class="spacer-4"></div>
+    <PhotoHeader />
+    <div class="spacer-4" />
 
-	<LinkShortener />
-	<div class="spacer-4"></div>
-</div>
+    <LinkShortener on:shortLinkCreated={handleShortLink} />
+    <div class="spacer-1" />
+
+    {#each links as link}
+      <LinkCopier {link} />
+      <div class="spacer-1" />
+    {/each}
+
+    <div class="spacer-4" />
+  </div>
 </main>
 
 <style>
-	.main-container {
-		padding-top: 2rem;
-		padding-left: 8rem;
-		padding-right: 8rem;
-	}
+  .main-container {
+    padding-top: 2rem;
+    padding-left: 8rem;
+    padding-right: 8rem;
+  }
 
-	.spacer-7 {
-		height: 7rem;
-	}
-	.spacer-4 {
-		height: 4rem;
-	}
+  .spacer-7 {
+    height: 7rem;
+  }
+  .spacer-4 {
+    height: 4rem;
+  }
+  .spacer-1 {
+    height: 1.5rem;
+  }
 </style>
--- src/components/LinkShortener.svelte
+++ src/components/LinkShortener.svelte
@@ -1,32 +1,48 @@
 <script lang="ts">
+  import { createEventDispatcher } from "svelte";
   import axios from "axios";
+  import type { ShortenedLink } from "../models/ShortenedLink";
+  import LoadingIcon from "./LoadingIcon.svelte";
+
+  const dispatch = createEventDispatcher();
   const baseUrl: string = "https://api.shrtco.de/v2/shorten?url=";
   let userInput: string = "";
   let shortLink: string = "";
   let errorMessage: string = "";
+  let loading: boolean = false;
 
   const getShortURL = (longURL: string): void => {
+    if (loading) {
+      return;
+    }
+    loading = true;
     const apiURL: string = baseUrl + longURL;
 
     axios
       .post(apiURL)
       .then((response) => {
         const data = response.data;
-        console.log("data", data);
         if (data.ok) {
-          console.log("short url: ", data.result.short_link);
           shortLink = data.result.short_link;
+          shortLinkCreated(longURL);
+          loading = false;
         }
       })
       .catch((error) => {
-        console.error("Error", error, '\nresponse: ', error.response);
+        console.error("Error", error, "\nresponse: ", error.response);
         if (error.response.data?.error) {
-            errorMessage = error.response.data?.error;
+          errorMessage = error.response.data?.error;
         } else {
-            errorMessage = error;
+          errorMessage = error;
         }
+        loading = false;
       });
   };
+
+  function shortLinkCreated(longLink: string) {
+    const link: ShortenedLink = { shortLink, longLink };
+    dispatch("shortLinkCreated", link);
+  }
 
   const handleKeydown = (event): void => {
     errorMessage = "";
@@ -36,6 +52,9 @@
   };
 
   const onSubmit = (): void => {
+    if (loading) {
+      return;
+    }
     if (userInput === "") {
       errorMessage = "Please enter a URL to shorten.";
       return;
@@ -59,18 +78,25 @@
       class="br-tight {errorMessage !== '' ? 'error' : ''}"
       id="userInput"
       aria-label="Link to shorten"
+      disabled={!!loading}
       bind:value={userInput}
       on:keydown={handleKeydown}
     />
-    <div class="btn btn-rounded-lg br-tight" on:click={onSubmit}>
-      Shorten it!
+    <div
+      class="btn btn-rounded-lg br-tight {loading ? 'btn-loading' : ''}"
+      on:click={onSubmit}
+    >
+      {#if loading}
+        <span class="spinner"><LoadingIcon /></span>
+      {:else}
+        Shorten it!
+      {/if}
     </div>
   </div>
   {#if errorMessage}
     <div class="error-message">{errorMessage}</div>
   {/if}
 </div>
-{shortLink}
 
 <style>
   .container {
@@ -93,6 +119,16 @@
 
   .btn {
     width: 6rem;
+    position: relative;
+  }
+
+  .btn-loading {
+    background-color: var(--gray-violet);
+    cursor: progress;
+  }
+
+  .btn-loading:hover {
+    color: white;
   }
 
   .error-message {
@@ -107,5 +143,11 @@
   .error {
     border: 3px solid var(--red);
     color: var(--red);
+  }
+
+  .spinner {
+    position: absolute;
+    top: 0rem;
+    left: 4rem;
   }
 </style>
